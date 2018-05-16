231c75067159b2e5d0ccd7f2b6176af4a30be783
manoelrui <manoelrui19@gmail.com>
2018-05-15 22:02:28 -0300

    Fixed python code style

--- judite.py
+++ judite.py
@@ -14,12 +14,12 @@ else:
     dbName = str(sys.argv[1]) + '.db'
 
 chatBot = ChatBot(
-        'Judite',
-        storage_adapter='chatterbot.storage.SQLStorageAdapter',
-        database='./database.sqlite3',
-        preprocessors=[
-            'chatterbot.preprocessors.clean_whitespace'
-        ]
+            'Judite',
+            storage_adapter='chatterbot.storage.SQLStorageAdapter',
+            database='./database.sqlite3',
+            preprocessors=[
+                'chatterbot.preprocessors.clean_whitespace'
+            ]
         )
 
 try:
@@ -29,9 +29,10 @@ except(KeyboardInterrupt, EOFError, SystemExit):
     print('Error - Could not connect with database')
 
 try:
-    cursor.execute("""
-    SELECT main FROM Conversation;
-    """        
+    cursor.execute(
+        """
+        SELECT main FROM Conversation;
+        """
     )
 except(KeyboardInterrupt, EOFError, SystemExit):
     print('Error - Could not get conversation data')
@@ -48,21 +49,20 @@ except(KeyboardInterrupt, EOFError, SystemExit):
     print('Error - Could not set trainning the dataset')
 
 print ('')
-print('Olá meu nome é %s, bem vindo a ZIM !!!' % chatBot.name);
+print('Olá meu nome é %s, bem vindo a ZIM !!!' % chatBot.name)
 print('')
 
 while True:
         try:
             request = input('YOU: ')
             response = chatBot.get_response(request)
-            
+
             if float(response.confidence > 0.4):
                 print('%s: %s' % (chatBot.name.upper(), response))
             else:
-                print('%s: Não entendi' % (chatBot.name.upper()));            
+                print('%s: Não entendi' % (chatBot.name.upper()))
             print('')
-        
+
         except(KeyboardInterrupt, EOFError, SystemExit):
             print('Error - Application error :(')
             break
-
